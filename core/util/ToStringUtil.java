cf236fe7680d13cc28a0abeeb43501afd745a920
hawk-tomy <67221751+hawk-tomy@users.noreply.github.com>
2023-12-25 15:12:23 +0900

    update arraylist format.

--- core/util/ToStringUtil.java
+++ core/util/ToStringUtil.java
@@ -6,14 +6,28 @@ import java.util.stream.Collectors;
 
 public class ToStringUtil {
     public static <T> String arrayList(ArrayList<T> list) {
-        return (
-            "(\n"
-            + list
-                .stream()
-                .map(Objects::toString)
-                .collect(Collectors.joining("\n"))
-                .indent(2)
-            + "\n)"
-        );
+        if (list.isEmpty()) {
+            return "()";
+        }
+        int length = list.size();
+        if (length < 5) {
+            return (
+                "(\n"
+                + list
+                    .stream()
+                    .map(Objects::toString)
+                    .collect(Collectors.joining("\n"))
+                    .indent(2)
+                + ")"
+            );
+        }
+        int d = (int) Math.ceil(Math.log10(length + 1));
+        StringBuilder sb = new StringBuilder("");
+        for (int i = 0; i < length; i++) {
+            sb.append(String.format("%0" + d + "d %s\n", i + 1, list.get(i)));
+        }
+        return "(\n" + sb
+            .toString()
+            .indent(2) + ")";
     }
 }
