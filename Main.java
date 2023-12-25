cc699f826ab60bc60066322d027be570a516bb2a
hawk-tomy <67221751+hawk-tomy@users.noreply.github.com>
2023-12-26 01:48:38 +0900

    main receive dir. search .wasm file in the dir.

--- Main.java
+++ Main.java
@@ -1,29 +1,52 @@
 import core.Parser;
 
+import java.io.File;
 import java.io.IOException;
-import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.stream.Stream;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("parse file count:" + args.length);
-        for (String pathStr : args) {
-            System.out.println("[start] parse: " + pathStr);
-            Path path = FileSystems
-                .getDefault()
-                .getPath(pathStr);
-            byte[] wasm;
-            try {
-                wasm = Files.readAllBytes(path);
-            } catch (IOException e) {
-                throw new RuntimeException(e); // unreachable or kill here.
-            }
+    int errCount;
 
-            Parser p = new Parser(wasm);
+    public static void main(String[] args) {
+        String dir = args[0];
+        var m = new Main();
+        try (Stream<Path> paths = Files.walk(Path.of(dir))) {
+            System.out.println(paths
+                .map(Path::toFile)
+                .filter(File::isFile)
+                .filter(f -> f
+                    .getName()
+                    .endsWith("wasm"))
+                .map(f -> {
+                    m.parseWasm(f);
+                    return 0;
+                })
+                .count());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println(m.errCount);
+    }
+
+    public void parseWasm(File path) {
+        byte[] wasm;
+        try {
+            wasm = Files.readAllBytes(path.toPath());
+        } catch (IOException e) {
+            throw new RuntimeException(e); // unreachable or kill here.
+        }
+
+        Parser p = new Parser(wasm);
+        try {
             var ret = p.parse();
-            System.out.println(ret);
-            System.out.println("[ end ] parse: " + pathStr);
+            // System.out.println(path);
+            // System.out.println(ret);
+        } catch (Exception e) {
+            this.errCount++;
+            e.printStackTrace(System.err);
+            System.err.println("index: " + p.getIndex());
         }
     }
 }
